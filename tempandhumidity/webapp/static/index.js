dddf9a669c8db6593979e0a910a541a8ae58fad4
bellfrank <franciscocw0101@gmail.com>
2022-05-27 02:26:13 -0700

    update

--- tempandhumidity/webapp/static/index.js
+++ tempandhumidity/webapp/static/index.js
@@ -4,35 +4,26 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-
-
+console.log("hello")
 const mariadb = require('mariadb');
 const pool = mariadb.createPool({
      host: 'localhost', 
-     user:'myUser', 
-     password: 'myPassword',
+     user:'frank', 
+     password: 'password',
+     database:'datalog',
      connectionLimit: 5
 });
-pool.getConnection()
-    .then(conn => {
-    
-      conn.query("SELECT 1 as val")
-        .then((rows) => {
-          console.log(rows); //[ {val: 1}, meta: ... ]
-          //Table must have been created before 
-          // " CREATE TABLE myTable (id int, val varchar(255)) "
-          return conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
-        })
-        .then((res) => {
-          console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
-          conn.end();
-        })
-        .catch(err => {
-          //handle error
-          console.log(err); 
-          conn.end();
-        })
-        
-    }).catch(err => {
-      //not connected
-    });
+
+async function asyncFunction() {
+  let conn;
+  try {
+	conn = await pool.getConnection();
+	const rows = await conn.query("SELECT * FROM thlog2 ORDER BY id DESC LIMIT 1");
+	console.log(rows);
+
+  } catch (err) {
+	throw err;
+  } finally {
+	if (conn) return conn.end();
+  }
+}
