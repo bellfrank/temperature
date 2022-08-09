525c8479b39a533e853bdeaecb47c5508a0be280
bellfrank <you@example.com>
2022-08-09 16:06:13 -0700

    new updates

--- tempandhumidity/webapp/app.py
+++ tempandhumidity/webapp/app.py
@@ -156,6 +156,7 @@ def analysis():
 
             return jsonify(info) # returning a JSON response
             
+
 @app.route("/lights", methods=["GET"])
 def lights():
     global light_status
@@ -235,16 +236,16 @@ def querytable():
 
         # Query table
         try:
-            cur.execute("SELECT * FROM thlog2 ORDER BY id DESC LIMIT 100")
+            cur.execute("SELECT * FROM thlog2 ORDER BY id DESC LIMIT 1000")
             templogs = []
             datelogs = []
             
             k = 0
             for (temperature, humidity, time, id) in cur:
                 pass
-                # k += 1
-                # if (k % 100 != 0):
-                #     continue
+                k += 1
+                if (k % 20 != 0):
+                    continue
                 
                 templogs.append(format(round(temperature, 2), '.2f'))
                 datelogs.append(time)
@@ -292,16 +293,16 @@ def queryhumidity():
 
         # Query table
         try:
-            cur.execute("SELECT * FROM thlog2 ORDER BY id DESC LIMIT 100")
+            cur.execute("SELECT * FROM thlog2 ORDER BY id DESC LIMIT 1000")
             humiditylogs = []
             datelogs = []
             
             k = 0
             for (temperature, humidity, time, id) in cur:
                 pass
-                # k += 1
-                # if (k % 100 != 0):
-                #     continue
+                k += 1
+                if (k % 20 != 0):
+                    continue
                 
                 humiditylogs.append(format(round(humidity, 2), '.2f'))
                 datelogs.append(time)
@@ -320,23 +321,133 @@ def queryhumidity():
         
         return jsonify(newlogs) # returning a JSON response
 
+def edge_detection(frame):
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        
+    # Use canny edge detection
+    edges = cv2.Canny(gray,50,150,apertureSize=3)
+    
+    # Apply HoughLinesP method to
+    # to directly obtain line end points
+    lines_list =[]
+    lines = cv2.HoughLinesP(
+                edges, # Input edge image
+                1, # Distance resolution in pixels
+                np.pi/180, # Angle resolution in radians
+                threshold=100, # Min number of votes for valid line
+                minLineLength=5, # Min allowed length of line
+                maxLineGap=10 # Max allowed gap between line for joining them
+                )
+    
+    # Iterate over points
+    for points in lines:
+        # Extracted points nested in the list
+        x1,y1,x2,y2=points[0]
+        # Draw the lines joing the points
+        # On the original image
+        cv2.line(frame,(x1,y1),(x2,y2),(0,255,0),2)
+        # Maintain a simples lookup list for points
+        lines_list.append([(x1,y1),(x2,y2)])
+    
+    return frame
 
-def gen_frames():
-    global camera
+
+def detect_grapes(frame):
+    # Convert it to gray
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    # Reduce the noise to avoid false circle detection
+    gray = cv2.medianBlur(gray, 5)
+    
+    # Proceed to apply Hough Circle Transform:
+    rows = gray.shape[0]
+    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, rows / 8, 
+    param1=100, param2=30, minRadius=0, maxRadius=40)
+    
+    # Draw the detected circles:
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+        for i in circles[0, :]:
+            center = (i[0], i[1])
+            # circle center
+            cv2.circle(frame, center, 1, (0, 100, 100), 3)
+            # circle outline
+            radius = i[2]
+            cv2.circle(frame, center, radius, (255, 0, 255), 3)
+    
+    return frame 
+
+# inverts frame colors
+def invertframes(frame):
+    frame = 255 - frame
+    return frame
+
+# convert greyscale
+def to_grey(frame):
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    return frame
+
+
+grey = False
+invert = False
+flip = False
+circle = False
+edge = False
+
+def flip_off():
+    global grey, invert, flip, circle, edge
+    grey = False
+    invert = False
+    flip = False
+    circle = False
+    edge = False
+
+
+@app.route("/radiobutton", methods=["POST"])
+def radio_button():
+    global grey, invert, flip, circle, edge
+    option = request.form.get("option")
+    print(option)
+    
+    flip_off()
+
+    if option == "grey":
+        grey = True
+    elif option == "circle":
+        circle = True
+    elif option == "invert":
+        invert = True
+    elif option == "edge":
+        edge = True
+    elif option == "flip":
+        flip = True
+
+    return ('', 204)
+
+
+def gen_frames(option=None):
+    global camera, grey, circle
+
     while True:
-        
-        success, frame = camera.read()  # read the camera frame
-        # # inverting frames colors
-        # frame = 255 - frame
-        
-        # # flipping an image
-        # frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-        # frame = np.flip(frame)
-        
-        # Converting image to grey per frame
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        
+        # read the camera frames
+        success, frame = camera.read()
 
+        if grey:
+            frame = to_grey(frame)
+        
+        elif invert:
+            frame = invertframes(frame)
+
+        elif flip:
+            frame = np.rot90(frame)
+        
+        elif circle:
+            frame = detect_grapes(frame)
+    
+        elif edge:
+            frame = edge_detection(frame)
+
+            
 
         if not success:
             break
@@ -365,6 +476,7 @@ def measure_temp():
         temp = os.popen("""vcgencmd measure_temp | awk -F "[=']" '{print($2 * 1.8)+32}'""").readline()
         print("inside measure temp")
         return jsonify(temp)
+
 
 def errorhandler(e):
     """Handle error"""
